0e8a5f90b9c45f0871c40fa9a6bfb3a397dedf1b
Paperios <sardykod@mai.ru>
2019-04-01 10:38:08 +0300

    Создание класса

---
+++ лаба 1/лаба 1/Obuv.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace лаба_1
+{
+    class Obuv
+    {
+        public string Name { get; set; } // Имя 
+        public float Razmer { get; set; } //Размер 
+        public double Cena { get; set; } //Цена 
+        public Obuv()
+        {
+            Name = null;
+            Razmer= 0;
+            Cena = 0;
+        }
+        public void GetInfo()
+        {
+            Console.WriteLine("Имя: {0} Размер: {1} Цена: {2}", Name, Razmer, Cena);
+        }
+        public void SetInfo()
+        {
+            Console.WriteLine("Введите имя: ");
+            Name = Console.ReadLine();
+            Console.WriteLine("Введите цена: ");
+            Razmer = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите размер: ");
+            Cena = Double.Parse(Console.ReadLine());
+        }
+    }
+
+
+
+}
---
+++ лаба 1/лаба 1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace лаба_1
+{
+    class Obu
+    {
+       
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("GIT");
+        }
+    }
+}
